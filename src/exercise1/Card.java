42fe55cf12f57ef4b4a88023ea971df50b17e3a8
Maria Ines Ponce de Leon <poncedel@sheridancollege.ca>
2023-05-25 11:19:40 -0400

    Cardtrick logic done

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -7,14 +7,15 @@ package exercise1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye
- * @author Paul Bonenfant May 2020
+ * 
+ * @author poncedel
+ * @author Maria Ines Ponce de Leon May 23, 2023 
  */
 public class Card {
 
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
-
+   
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
     /**
      * @return the suit
@@ -42,5 +43,5 @@ public class Card {
      */
     public void setValue(int value) {
         this.value = value;
-    }  
+    }     
 }
--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,28 +1,63 @@
 package exercise1;
 
+import java.util.Scanner;
+import java.util.Random;
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author poncedel
+ * @author Maria Ines Ponce de Leon May 23, 2023 
  */
 public class CardTrick {
     
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
-
+        Random random = new Random();
+                
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
+            int randomNumber = random.nextInt(13) + 1;
             //card.setValue(insert call to random number generator here)
-            // 
+            card.setValue(randomNumber);
+            
+            int randomSuit = random.nextInt(4);
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            card.setSuit(Card.SUITS[randomSuit]);
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
+            
+            hand[i] = card;
         }
 
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Input a card value (1-10, Jack-11, Queen-12, King-13): ");
+        int cardValue = sc.nextInt();
+        
+        System.out.print("Input a card suit (0-Diamonds, 1-Hearts, 2-Spades, 3-Clubs): ");
+        int cardSuit = sc.nextInt();
+        
+        Card user = new Card();
+        user.setValue(cardValue);
+        user.setSuit(Card.SUITS[cardSuit]);
+        
+        boolean isAMatch = false;
+        
+        for (Card card : hand) {
+            if (card.getValue() == user.getValue() && card.getSuit().equals(user.getSuit())) {
+                isAMatch = true;
+                break;
+            }
+        }
+        
+        if (isAMatch) {
+            printInfo();
+        } else {
+            System.out.println("Sorry, your card was not found. Try again!");
+        }
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
